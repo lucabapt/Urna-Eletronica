9294eae4ae3d2ce2280b5f6ea91cb15c380e3dba
Lucas <Lucas@senai>
2023-10-06 16:42:02 -0300

    feat:(inicio do programa urna eletronica)

---
+++ urnaeletronica.js
@@ -0,0 +1,43 @@
+// Estrutura: switch-case
+
+const opcao = 1;
+
+switch(opcao) {
+
+case 1:
+    console.log('Opção é 1');
+    break;
+
+    case 2:
+    console.log('Opção é 2')
+    break;
+
+    default:
+        console.log('Nenhuma das opções');
+}
+
+// Estrutura de repetição: do-while (faça enquanto)
+//do {
+  //  console.log('instrução 1')
+  //  console.log('instrução 2')
+  //  console.log('instrução 3')
+ //   console.log('instrução 4')
+  //  console.log('instrução 5')
+  //  console.log('instrução 6')
+
+// } while (contador ! == 1);
+
+
+
+while (contador !== 0);
+{
+    console.log('instrução 1')
+    console.log('instrução 2')
+    console.log('instrução 3')
+    console.log('instrução 4')
+    console.log('instrução 5')
+    console.log('instrução 6')
+
+} 
+
+

63b0a908f96c77e4f39fafd0b6bef153cb45e5f5
Lucas <Lucas@senai>
2023-10-11 16:30:19 -0300

    feat:(inserindo as formulas da urna eletronica)

--- urnaeletronica.js
+++ urnaeletronica.js
@@ -1,43 +1,100 @@
-// Estrutura: switch-case
+function urnaEletronica() {
 
-const opcao = 1;
+// Aqui vai todo o codigo do programa....
 
-switch(opcao) {
+console.log('Iniciando o programa');
 
-case 1:
-    console.log('Opção é 1');
-    break;
+
+let
+
+  totalVotosCandidato1 = 0,
+  totalVotosCandidato2 = 0,
+  totalVotosCandidato3 = 0,
+  totalVotosBranco = 0,
+  totalVotosNulo = 0;
+
+console.log(totalVotosCandidato1);
+console.log(totalVotosCandidato2);
+console.log(totalVotosCandidato3);
+console.log(totalVotosBranco);
+console.log(totalVotosNulo);
+
+do {
+      // Instruções repetidas no Loop \\ 
+
+console.log('|1| Candidato 1');
+console.log('|2| Candidato 2');
+console.log('|3| Candidato 3');
+console.log('|5| Branco');
+console.log('|8| Nulo');
+console.log('|0| Encerrar a votação');
+
+voto = parseInt(prompt('Digite sua opção de voto'));
+
+  switch (voto) {
+
+    case 1:
+      totalVotosCandidato1++;
+      console.log('O candidato 1 recebeu um voto');
+      break; 
 
     case 2:
-    console.log('Opção é 2')
-    break;
+      totalVotosCandidato2++;
+      console.log('O candidato 2 recebeu um voto');
+      break;
+      
+    case 3:  
+      totalVotosCandidato3++;
+      console.log('O candidato 3 recebeu um voto');
+      break;
 
-    default:
-        console.log('Nenhuma das opções');
+    case 5:
+      totalVotosBranco++
+      console.log('O branco recebeu 1 voto');
+      break;
+
+    case 8:
+      totalVotosNulo++
+      console.log('O nulo recebeu 1 voto');
+      break;
+      
+    case 0:
+      console.log('Encerrar o programa')  
+      default:
+       
+
+    if (voto === 1){
+      totalVotosCandidato1++;
+      console.log('O candidato 1 recebeu um voto');
+
+    } else if (voto === 2) {
+      totalVotosCandidato2++;
+      console.log('O candidato 2 recebeu um voto');
+
+    } else if (voto === 3) {
+        totalVotosCandidato3++;
+        console.log('O candidato 3 recebeu um voto');
+      
+    } else if (voto === 5) {
+        totalVotosBranco5++;
+        console.log('Um voto em branco');
+        
+      } else if (voto === 8) {
+        totalVotosNulo++;
+        console.log('Um voto nulo');      
+
+
+    }
+
+    return;
+
+  }
+
+} while (voto !==0);
+
+console.clear
+
 }
 
-// Estrutura de repetição: do-while (faça enquanto)
-//do {
-  //  console.log('instrução 1')
-  //  console.log('instrução 2')
-  //  console.log('instrução 3')
- //   console.log('instrução 4')
-  //  console.log('instrução 5')
-  //  console.log('instrução 6')
-
-// } while (contador ! == 1);
-
-
-
-while (contador !== 0);
-{
-    console.log('instrução 1')
-    console.log('instrução 2')
-    console.log('instrução 3')
-    console.log('instrução 4')
-    console.log('instrução 5')
-    console.log('instrução 6')
-
-} 
 
 

20839f4c73de184de43bcdb8a92ce21349833f90
Lucas <Lucas@senai>
2023-10-18 16:45:28 -0300

    feat:(adicionando tofixed a minha urna eletronica)

--- urnaeletronica.js
+++ urnaeletronica.js
@@ -11,13 +11,20 @@ let
   totalVotosCandidato2 = 0,
   totalVotosCandidato3 = 0,
   totalVotosBranco = 0,
-  totalVotosNulo = 0;
+  totalVotosNulo = 0,
+  nomecandidato1, 
+  nomecandidato2, 
+  nomecandidato3;
 
 console.log(totalVotosCandidato1);
 console.log(totalVotosCandidato2);
 console.log(totalVotosCandidato3);
 console.log(totalVotosBranco);
 console.log(totalVotosNulo);
+
+nomecandidato1 = prompt  ('Nome do Candidato 1');
+nomecandidato2 = prompt  ('Nome do Candidato 2');
+nomecandidato3 = prompt  ('Nome do Candidato 3');
 
 do {
       // Instruções repetidas no Loop \\ 
@@ -29,23 +36,24 @@ console.log('|5| Branco');
 console.log('|8| Nulo');
 
 
+
 voto = parseInt(prompt('Digite sua opção de voto'));
 
   switch (voto) {
 
     case 1:
       totalVotosCandidato1++;
-      console.log('O candidato 1 recebeu um voto');
+      console.log("O", nomecandidato1 , "recebeu um voto");
       break; 
 
     case 2:
       totalVotosCandidato2++;
-      console.log('O candidato 2 recebeu um voto');
+      console.log("O", nomecandidato2, "recebeu um voto");
       break;
       
     case 3:  
       totalVotosCandidato3++;
-      console.log('O candidato 3 recebeu um voto');
+      console.log("O", nomecandidato3, "recebeu um voto");
       break;
 
     case 5:
@@ -65,24 +73,25 @@ voto = parseInt(prompt('Digite sua opção de voto'));
 
     console.log ("O programa esta encerrado")
 
-    console.log ("O total de votos é", totalVotosCandidato1+totalVotosCandidato2+totalVotosCandidato3+totalVotosBranco+totalVotosNulo);
-    console.log ("O total de votos do candidato 1 é" , totalVotosCandidato1);
-    console.log ("O total de votos do candidato 2 é" , totalVotosCandidato2);
-    console.log ("O total de votos do candidato 3 é" , totalVotosCandidato3);
-    console.log ("O total de votos em Branco é" , totalVotosBranco);
-    console.log ("O total de votos Nulo é" , totalVotosNulo);
-    console.log ("O total de votos validos é" , totalVotosCandidato1+totalVotosCandidato2+totalVotosCandidato3);
-    console.log ("% de votos no candidato 1 é", totalVotosCandidato1/(totalVotosCandidato1+totalVotosCandidato2+totalVotosCandidato3)*100 + "%");
-    console.log ("% de votos no candidato 2 é", totalVotosCandidato2/(totalVotosCandidato1+totalVotosCandidato2+totalVotosCandidato3)*100 + "%");
-    console.log ("% de votos no candidato 3 é", totalVotosCandidato3/(totalVotosCandidato1+totalVotosCandidato2+totalVotosCandidato3)*100 + "%");
-    console.log ("% de votos em Branco é", totalVotosBranco/totalVotosCandidato1+totalVotosCandidato2+totalVotosCandidato3+totalVotosBranco+totalVotosNulo*100 + "%");
-    console.log ("% de votos Nulo é", totalVotosNulo/totalVotosCandidato1+totalVotosCandidato2+totalVotosCandidato3+totalVotosBranco+totalVotosNulo *100 + "%");
+    console.log ("O total de votos é \n", totalVotosCandidato1+totalVotosCandidato2+totalVotosCandidato3+totalVotosBranco+totalVotosNulo);
 
+    console.log ("O total de votos do",  nomecandidato1, "é\n" , totalVotosCandidato1);
+    console.log ("% de votos no" , nomecandidato1 ,  "é \n", (totalVotosCandidato1/(totalVotosCandidato1+totalVotosCandidato2+totalVotosCandidato3)*100 + "%").toFixed(2));
 
+    console.log ("O total de votos do" , nomecandidato2 , "é \n" , totalVotosCandidato2);
+    console.log ("% de votos no", nomecandidato2 ,"é \n", totalVotosCandidato2/(totalVotosCandidato1+totalVotosCandidato2+totalVotosCandidato3)*100 + "%").toFixed(2);
 
+    console.log ("O total de votos do", nomecandidato3 ,"é \n" , totalVotosCandidato3);
+    console.log ("% de votos no", nomecandidato3, "é \n", totalVotosCandidato3/(totalVotosCandidato1+totalVotosCandidato2+totalVotosCandidato3)*100 + "%").toFixed(2);
+    
+    console.log ("O total de votos em Branco é \n" , totalVotosBranco);
+    console.log ("% de votos em Branco é \n", totalVotosBranco/(totalVotosCandidato1+totalVotosCandidato2+totalVotosCandidato3+totalVotosBranco+totalVotosNulo)*100 + "%").toFixed(2);
 
+    console.log ("O total de votos Nulo é \n" , totalVotosNulo);
+    console.log ("% de votos Nulo é \n", totalVotosNulo/(totalVotosCandidato1+totalVotosCandidato2+totalVotosCandidato3+totalVotosBranco+totalVotosNulo) *100 + "%").toFixed(2);
 
-
+    console.log ("O total de votos validos é \n" , totalVotosCandidato1+totalVotosCandidato2+totalVotosCandidato3);
+    
     if (voto === 1){
       totalVotosCandidato1++;
       console.log('O candidato 1 recebeu um voto');
